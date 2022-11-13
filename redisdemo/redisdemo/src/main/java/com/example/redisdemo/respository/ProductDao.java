291575b6fbcd4c96b7954438c50a506169e57ab9
hahuyvu975 <hahuyvu975@gmail.com>
2022-11-13 17:23:10 +0700

    Chua hoan thanh xong cofig vs redis

---
+++ redisdemo/redisdemo/src/main/java/com/example/redisdemo/respository/ProductDao.java
@@ -0,0 +1,14 @@
+package com.example.redisdemo.respository;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductDao {
+	
+	private RedisTemplate template;
+	
+//	public Product save(Product product) {
+//		
+//	}
+}
